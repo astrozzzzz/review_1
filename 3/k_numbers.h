0793eecb8a3920264ed644e9d84cdc46fa5fb463
astrozzzzz <dcherepulya1@mail.ru>
2025-04-04 11:38:45 +0500

    second

--- 3/k_numbers.h
+++ 3/k_numbers.h
@@ -2,9 +2,15 @@
 #define K_NUMBERS_H
 
 #include <iostream>
+#include <iomanip>
 // FIX ME ненужный импорт
 //#include <cmath>
 
-int main();
+
+bool ReadInput(int& k, int& n);
+
+bool ValidateInput(int k, int n);
+
+double ComputeResult(int k, int n);
 
 #endif
--- 4/calculator.cpp
+++ 4/calculator.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/**
 * Во всех заданиях данной подгруппы предполагается, что исходные строки, определяющие выражения, 
 * не содержат пробелов.При выполнении заданий не следует использовать оператор цикла.
 * Вывести значение целочисленного выражения, заданного в виде строки S.Выражение
@@ -7,132 +7,95 @@
 * < знак > :: = +| − | *
 */
 
-// FIX ME: фигурные скобки должны быть на новой строке в строках 17, 21, 23, 27, 38, 42, 44, 47, 54, 58, 60, 63, 65, 67, 76, 80
-
 #include "calculator.h"
 
 using namespace std;
 
-// @breif Объявление функции для вычисления выражения
-// FIX ME функция должна быть в snake case
-// int Calc(string s);
-int calc(const string& s);
 
-/*
+/**
  * @brief Преобразует строку в число или вычисляет вложенное выражение.
  * @param s Входная строка, представляющая число или выражение в скобках.
  * @return Целочисленное значение выражения.
  */
- // FIX ME функция должна быть в snake case и иметь понятное название
+// FIX ME функция должна иметь понятное название
 // int Num(string s)
-int parse_number(const string& s)
-{
-	if (s[0] == '(' && s[s.length() - 1] == ')')
-	{
-		return calc(s.substr(1, s.length() - 2));
-	}
-	else
-	{
-		return stoi(s);
-	}
+int ParseNumber(const string& s) {
+    if (s[0] == '(' && s[s.length() - 1] == ')') {
+        return Calc(s.substr(1, s.length() - 2));
+    }
+    else {
+        return stoi(s);
+    }
 }
 
-/*
+/**
  * @brief Вычисляет произведение двух подвыражений, разделённых знаком '*'.
  * @param s Входная строка, представляющая выражение.
  * @return Результат умножения.
  */
- // FIX ME функция должна быть в snake case и иметь понятное название
+// FIX ME функция должна иметь понятное название
 // int Term(string s)
-int compute_product(const string& s)
-{
-	int i = s.length() - 1;
-	int level = 0;
-	int pos = -1;
-	while (i >= 0)
-	{
-		if (s[i] == ')')
-		{
-			level++;
-		}
-		else if (s[i] == '(')
-		{
-			level--;
-		}
-		else if (level == 0 && s[i] == '*')
-		{
-			pos = i;
-			break;
-		}
-		i--;
-	}
+int ComputeProduct(const string& s) {
+    int i = s.length() - 1;
+    int level = 0;
+    int pos = -1;
+    while (i >= 0) {
+        if (s[i] == ')') {
+            level++;
+        }
+        else if (s[i] == '(') {
+            level--;
+        }
+        else if (level == 0 && s[i] == '*') {
+            pos = i;
+            break;
+        }
+        i--;
+    }
 
-	if (pos == -1)
-	{
-		return parse_number(s);
-	}
+    if (pos == -1) {
+        return ParseNumber(s);
+    }
 
-	return compute_product(s.substr(0, pos)) * parse_number(s.substr(pos + 1));
+    return ComputeProduct(s.substr(0, pos)) * ParseNumber(s.substr(pos + 1));
 }
 
-/*
+/**
  * @brief Рекурсивно вычисляет значение целочисленного выражения.
  * @param s Входная строка, представляющая выражение.
  * @return Результат вычисления.
  */
-int calc(const string& s)
-{
-	int i = s.length() - 1;
-	int level = 0;
-	int pos = -1;
-	while (i >= 0)
-	{
-		if (s[i] == ')')
-		{
-			level++;
-		}
-		else if (s[i] == '(')
-		{
-			level--;
-		}
-		else if (level == 0 && (s[i] == '+' || s[i] == '-'))
-		{
-			pos = i;
-			break;
-		}
-		i--;
-	}
+// FIX ME функция должна объявляться в заголовочном файле
+// int Calc(string s);
+int Calc(const string& s) {
+    int i = s.length() - 1;
+    int level = 0;
+    int pos = -1;
+    while (i >= 0) {
+        if (s[i] == ')') {
+            level++;
+        }
+        else if (s[i] == '(') {
+            level--;
+        }
+        else if (level == 0 && (s[i] == '+' || s[i] == '-')) {
+            pos = i;
+            break;
+        }
+        i--;
+    }
 
-	if (pos == -1)
-	{
-		return compute_product(s);
-	}
+    if (pos == -1) {
+        return ComputeProduct(s);
+    }
 
-	char op = s[pos];
-	if (op == '+')
-	{
-		return calc(s.substr(0, pos)) + compute_product(s.substr(pos + 1));
-	}
-	else if (op == '-')
-	{
-		return calc(s.substr(0, pos)) - compute_product(s.substr(pos + 1));
-	}
+    char op = s[pos];
+    if (op == '+') {
+        return Calc(s.substr(0, pos)) + ComputeProduct(s.substr(pos + 1));
+    }
+    else if (op == '-') {
+        return Calc(s.substr(0, pos)) - ComputeProduct(s.substr(pos + 1));
+    }
 
-	return 0;
-}
-
-/*
- * @brief Основная функция программы для вычисления целочисленного выражения.
- * Считывает строку с выражением и выводит его значение.
- * @return Код завершения программы (0 при успехе).
- */
-int main()
-{
-	setlocale(0, "");
-	string s;
-	cout << "Введите строку вида - <цифра> | (<выражение><знак><выражение>), где знак - (+ | - | *):" << endl;
-	cin >> s;
-	cout << "Результат: " << calc(s) << endl;
-
-	return 0;
+    return 0;
 }
